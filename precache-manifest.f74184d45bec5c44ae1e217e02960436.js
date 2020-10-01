5d3edb2987ccb7404a1c2b5dd7fc7542c504b8ff
jaisharx <sendtojsharma@gmail.com>
2020-10-01 13:35:11 +0000

    Deploying to gh-pages from  @ 30bbf4b68926459a8b02c72e76bd16d0befdac61 🚀

---
+++ precache-manifest.f74184d45bec5c44ae1e217e02960436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcff692292aadad088662f85aeeb8db6",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "36d66c1f1678cbe85789",
+    "url": "/react-expense-tracker/static/css/main.33eecf31.chunk.css"
+  },
+  {
+    "revision": "89c547222e9b52b21084",
+    "url": "/react-expense-tracker/static/js/2.d5a5e238.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.d5a5e238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d66c1f1678cbe85789",
+    "url": "/react-expense-tracker/static/js/main.78e331ac.chunk.js"
+  },
+  {
+    "revision": "78c83d7ef0dc2332c961",
+    "url": "/react-expense-tracker/static/js/runtime-main.f8b53f9c.js"
+  }
+]);
