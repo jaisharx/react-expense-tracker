630c0deb401c4b65715f1b5065e7dbf7c9dc3984
jai-sharma <sendtojsharma@gmail.com>
2020-10-01 18:52:21 +0530

    added more stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import Header from './components/Header';
-import Balance from './components/Balance';
-import IncomeExpenses from './components/IncomeExpenses';
-import TranscationList from './components/TranscationList';
-import AddTranscation from './components/AddTranscation';
+import Header from './Header';
+import Balance from './Balance';
+import IncomeExpenses from './IncomeExpenses';
+import TranscationList from './TranscationList';
+import AddTranscation from './AddTranscation';
 
 function App() {
     return (
